4a675b0e541c9beb3c6a478bfa97900369e05f66
NowApoca <manueldiez123@gmail.com>
2019-12-04 17:39:10 -0300

    Added refresj and tooltips

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -1,41 +1,147 @@
-function renderChart(data, data2, data3, data4, data5, labels) {
 
-    var timeFormat = "MM/DD/YYYY HH:mm";
-    var ctx1 = document.getElementById("myChart1").getContext('2d');
-    var ctx4 = document.getElementById("myChart4").getContext('2d');
-    var myChart4 = new Chart(ctx4, {
-        type: 'doughnut',
-        data: {
-            labels: ["gas", "coal", "nuclear"],
-            datasets: [{
-                data: [13000, 9000, 7000],
-                borderColor: ['rgba(124, 109, 109, 1)', 'rgba(10, 67, 72, 1)', 'rgba(206, 245, 0, 1)'],
-                backgroundColor: ['rgba(124, 109, 109, 0.2)', 'rgba(10, 67, 72, 0.2)', 'rgba(206, 245, 0, 0.2)'],
-            }]
-        },
-    });
-    var myChart1 = new Chart(ctx1, {
+data = [20000, 14000, 12000, 11000, 12000, 14000, 13000];
+data2 = [15000, 10000, 9000, 12000, 14000, 10000, 9000];
+data3 = [10000, 8000, 5000, 8000, 7000, 9000, 7000];
+data4 = [8000, 12000, 12000, 8000, 8000, 19000, 22000];
+data5 = [5000, 5000, 5000, 5000, 5000, 5000, 5000];
+
+const graphs = [{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart1",
+    type:"line",
+    yAxisLabel: "Output usale by fuel type",
+    xAxisLabel: "Settlement Period",
+    yAxisUnit: "MW",
+    xAxisUnit: "",
+    labels: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"],
+    datasets: [{
+        label: 'Gas',
+        data: data,
+        fill: "1",
+        borderColor: 'rgba(124, 109, 109, 1)',
+        backgroundColor: 'rgba(124, 109, 109, 0.9)',
+    },{
+        label: 'Coal',
+        data: data2,
+        fill: "2",
+        borderColor: 'rgba(10, 67, 72, 1)',
+        backgroundColor: 'rgba(10, 67, 72, 0.9)',
+    },{
+        label: 'Nuclear',
+        data: data3,
+        fill: "origin",
+        borderColor: 'rgba(206, 245, 0, 1)',
+        backgroundColor: 'rgba(206, 245, 0, 0.9)',
+    }]
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart2",
+    type:"doughnut",
+    yAxisUnit: "MW",
+    labels: ["gas", "coal", "nuclear"],
+    datasets: [{
+        data: [13000, 9000, 7000],
+        borderColor: ['rgba(124, 109, 109, 1)', 'rgba(10, 67, 72, 1)', 'rgba(206, 245, 0, 1)'],
+        backgroundColor: ['rgba(124, 109, 109, 0.2)', 'rgba(10, 67, 72, 0.2)', 'rgba(206, 245, 0, 0.2)'],
+    }]
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart3",
+    type:"line",
+    yAxisLabel: "System Prices",
+    xAxisLabel: "Settlement Period",
+    yAxisUnit: "$/MWh",
+    xAxisUnit: "",
+    labels: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"],
+    datasets: [{
+        data: data,
+        borderColor: 'rgba(124, 109, 109, 1)',
+        backgroundColor: 'rgba(124, 109, 109, 0.2)',
+        fill: {display: false}
+    }]
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart4",
+    type:"line",
+    yAxisLabel: "Market Imbalance",
+    xAxisLabel: "Settlement Period",
+    yAxisUnit: "MWh",
+    xAxisUnit: "",
+    labels: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart5",
+    type:"line",
+    yAxisLabel: "System Demand",
+    xAxisLabel: "Settlement Period",
+    yAxisUnit: "MW",
+    xAxisUnit: "",
+    labels: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart6",
+    type:"line",
+    yAxisLabel: "Rolling System Demand",
+    xAxisLabel: "Settlement Period",
+    yAxisUnit: "MW",
+    xAxisUnit: "",
+    labels: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart7",
+    type:"line",
+    yAxisLabel: "Average Half Hourly Interconnector Flows",
+    xAxisLabel: "Settlement Period",
+    yAxisUnit: "MW",
+    xAxisUnit: "",
+    labels: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart8",
+    type:"line",
+    yAxisLabel: "Rolling System Frequency",
+    xAxisLabel: "Settlement Period",
+    yAxisUnit: "Hz",
+    xAxisUnit: "",
+    labels: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+}]
+
+function floatToUnit(value, unit) {
+    return unit + (value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+for(const graph of graphs){
+    renderChart(graph);
+}
+
+function renderChart(graph) {
+    let ctx = document.getElementById(graph.id).getContext('2d');
+    if(graph.type == "line"){
+        createNormalLineZoomChart(graph, ctx);
+    }else if(graph.type == "doughnut"){
+        new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: graph.labels,
+                datasets: graph.datasets
+            },
+        });
+    }
+    
+}
+
+function resetZoom(){
+
+    var ctx8 = document.getElementById("myChart8").getContext('2d');
+
+    var myChart8 = new Chart(ctx8, {
         type: 'line',
         data: {
             labels: labels,
             datasets: [{
-                label: 'Gas',
-                data: data,
-                fill: "1",
+                data: data4,
                 borderColor: 'rgba(124, 109, 109, 1)',
-                backgroundColor: 'rgba(124, 109, 109, 0.9)',
-            },{
-                label: 'Coal',
-                data: data2,
-                fill: "2",
-                borderColor: 'rgba(10, 67, 72, 1)',
-                backgroundColor: 'rgba(10, 67, 72, 0.9)',
-            },{
-                label: 'Nuclear',
-                data: data3,
-                fill: "origin",
-                borderColor: 'rgba(206, 245, 0, 1)',
-                backgroundColor: 'rgba(206, 245, 0, 0.9)',
+                backgroundColor: 'rgba(124, 109, 109, 0.2)',
             }]
         },
         options: {
@@ -78,7 +184,6 @@ function renderChart(data, data2, data3, data4, data5, labels) {
                     }
                 }
             },
-
             scales: {
                 yAxes: [
                     {
@@ -87,20 +192,25 @@ function renderChart(data, data2, data3, data4, data5, labels) {
                             display: true
                         },
                         scaleLabel: {
-                            display: false
-                        },
+                            display: true,
+                            labelString: 'Consumption'
+                          },
                         ticks: {
                             display: true,
                             beginAtZero: true,
-                            max: 60000
+                            max: 60000,
+                            callback: function (value, index, values) {
+                                return float2dollar(value);
+                            }
                         }
                     }
                 ],
                 xAxes: [{
                     stacked: true,
                     scaleLabel: {
-                        display: false
-                    },
+                        display: true,
+                        labelString: 'Time'
+                      },
                     ticks: {
                         display: true,
                         min: 'January-17',
@@ -110,21 +220,16 @@ function renderChart(data, data2, data3, data4, data5, labels) {
             }
         }
     });
+    myChart8.resetZoom()
 }
 
-data = [20000, 14000, 12000, 11000, 12000, 14000, 13000];
-data2 = [15000, 10000, 9000, 12000, 14000, 10000, 9000];
-data3 = [10000, 8000, 5000, 8000, 7000, 9000, 7000];
-data4 = [8000, 12000, 12000, 8000, 8000, 19000, 22000];
-data5 = [5000, 5000, 5000, 5000, 5000, 5000, 5000];
-labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-renderChart(data, data2, data3, data4, data5, labels);
-
-window.onload = function () {
-    var ctx = document.getElementById("canvas").getContext("2d");
-    window.myBar = new Chart(ctx, {
+function createNormalLineZoomChart(graph, documentById){
+    new Chart(documentById, {
         type: 'line',
-        data: barChartData,
+        data: {
+            labels: graph.labels,
+            datasets: graph.datasets
+        },
         options: {
             // Elements options apply to all of the options unless overridden in a dataset
             // In this case, we are setting the border of each bar to be 2px wide and green
@@ -148,26 +253,23 @@ window.onload = function () {
                 zoom: {
                     pan: {
                         enabled: true,
-                        mode: 'xy'
+                        mode: 'y'
                     },
                     zoom: {
                         enabled: true,
-                        mode: 'xy'
+                        mode: 'y'
                     },
 
                     rangeMin: {
                         // Format of min zoom range depends on scale type
-                        x: null,
                         y: 0
                     },
                     rangeMax: {
                         // Format of max zoom range depends on scale type
-                        x: null,
-                        y: 16000
+                        y: 160000
                     }
                 }
             },
-
             scales: {
                 yAxes: [
                     {
@@ -176,18 +278,25 @@ window.onload = function () {
                             display: true
                         },
                         scaleLabel: {
-                            display: false
-                        },
+                            display: true,
+                            labelString: graph.yAxisLabel
+                          },
                         ticks: {
-                            display: true
+                            display: true,
+                            beginAtZero: true,
+                            max: 60000,
+                            callback: function (value, index, values) {
+                                return floatToUnit(value, graph.yAxisUnit);
+                            }
                         }
                     }
                 ],
                 xAxes: [{
                     stacked: true,
                     scaleLabel: {
-                        display: false
-                    },
+                        display: true,
+                        labelString: 'Time'
+                      },
                     ticks: {
                         display: true,
                         min: 'January-17',
@@ -197,30 +306,4 @@ window.onload = function () {
             }
         }
     });
-};
-
-
-var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var randomScalingFactor = function () {
-    return (Math.random() > 0.5 ? 1.0 : 1.0) * Math.round(Math.random() * 10000);
-};
-var randomColorFactor = function () {
-    return Math.round(Math.random() * 255);
-};
-var randomColor = function () {
-    return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',.7)';
-};
-
-var barChartData = {
-    labels: ["January-16", "February-16", "March-16", "April-16", "May-16", "June-16", "July-16", "August-16", "September-16", "October-16", "November-16", "December-16", "January-17", "February-17", "March-17", "April-17", "May-17", "June-17", "July-17", "August-17", "September-17", "October-17", "November-17", "December-17"],
-    datasets: [{
-        label: 'Credit Card',
-        backgroundColor: "rgba(133,169,69,0.9)",
-        data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-    }, {
-        label: 'Cash',
-        backgroundColor: "rgba(151,187,205,0.5)",
-        data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-    }]
-
-};
+}
--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,68 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', supplyStats: [{
-    "timestamp" : 1560384000,
-    "circulating_supply" : 2501386543555556.0,
-    "tradeable_stake" : 2387555555555.56,
-    "non_participating_stake" : 9018613456444444.0,
-    "participating_stake" : 981386543555556.0},{
-    "timestamp" : 1570384000,
-    "circulating_supply" : 2501386543555556.0,
-    "tradeable_stake" : 2387555555555.56,
-    "non_participating_stake" : 9018613456444444.0,
-    "participating_stake" : 981386543555556.0},{
-    "timestamp" : 1580384000,
-    "circulating_supply" : 2501386543555556.0,
-    "tradeable_stake" : 2387555555555.56,
-    "non_participating_stake" : 9018613456444444.0,
-    "participating_stake" : 981386543555556.0}] });
+  res.render('index', { title: 'Express', graphs: [{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart1",
+    type:"line",
+    yAxisLabel: "Output usale by fuel type",
+    xAxisLabel: "Settlement Period",
+    yAxisUnity: "U$",
+    xAxisUnity: "",
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart2",
+    type:"donnut",
+    yAxisUnity: "MW",
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart3",
+    type:"line",
+    yAxisLabel: "System Prices",
+    xAxisLabel: "Settlement Period",
+    yAxisUnity: "$/MWh",
+    xAxisUnity: "",
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart4",
+    type:"line",
+    yAxisLabel: "Market Imbalance",
+    xAxisLabel: "Settlement Period",
+    yAxisUnity: "MWh",
+    xAxisUnity: "",
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart5",
+    type:"line",
+    yAxisLabel: "System Demand",
+    xAxisLabel: "Settlement Period",
+    yAxisUnity: "MW",
+    xAxisUnity: "",
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart6",
+    type:"line",
+    yAxisLabel: "Rolling System Demand",
+    xAxisLabel: "Settlement Period",
+    yAxisUnity: "MW",
+    xAxisUnity: "",
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart7",
+    type:"line",
+    yAxisLabel: "Average Half Hourly Interconnector Flows",
+    xAxisLabel: "Settlement Period",
+    yAxisUnity: "MW",
+    xAxisUnity: "",
+  },{
+    title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
+    id:"myChart8",
+    type:"line",
+    yAxisLabel: "Rolling System Frequency",
+    xAxisLabel: "Settlement Period",
+    yAxisUnity: "Hz",
+    xAxisUnity: "",
+  }] });
 });
 
 module.exports = router;
