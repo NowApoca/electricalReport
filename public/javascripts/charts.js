72508c1e7b8f8f0d68ee254ecbcc24678813a993
Pukitala <manueldiez123@gmail.com>
2019-12-06 01:29:15 -0300

    Added graphs and redesign the website

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -1,4 +1,3 @@
-
 data = [20000, 14000, 12000, 11000, 12000, 14000, 13000];
 data2 = [15000, 10000, 9000, 12000, 14000, 10000, 9000];
 data3 = [10000, 8000, 5000, 8000, 7000, 9000, 7000];
@@ -122,18 +121,38 @@ const graphs = [{
   },{
     title:"Many users already have downloaded Bootstrap from MaxCDN when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.",
     id:"myChart8",
-    type:"line",
-    yAxisLabel: "Rolling System Frequency",
-    xAxisLabel: "Settlement Period",
-    yAxisUnit: "Hz",
-    xAxisUnit: "",
-    labels: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"],
+    type:"doughnut",
+    yAxisUnit: "MW",
+    labels: ["LOW", "MEDIUM", "HIGH"],
     datasets: [{
-        data: data,
-        borderColor: 'rgba(124, 109, 109, 1)',
-        backgroundColor: 'rgba(124, 109, 109, 0.2)',
-        fill: {display: false}
-    }]
+        data: [9000, 9000, 9000],
+        borderColor: ['rgba(33, 175, 33, 1)', 'rgba(175, 175, 33, 1)', 'rgba(175, 33, 33, 1)'],
+        backgroundColor: ['rgba(33, 175, 33, 1)', 'rgba(175, 175, 33, 1)', 'rgba(175, 33, 33, 1)'],
+    },
+    {
+        "data": [
+            300,
+            200,
+            5700,
+        ],
+        "backgroundColor": [
+            "rgba(0, 0, 0, 0)",
+            "rgba(0, 0, 0, 0.6)",
+            "rgba(0, 0, 0, 0)"
+        ],
+        "borderWidth": 0,
+        "hoverBackgroundColor": [
+            "rgba(0, 0, 0, 0)",
+            "rgba(0, 0, 0, 0.6)",
+            "rgba(0, 0, 0, 0)"
+        ],
+        "hoverBorderWidth": 0
+    }],
+    options: {
+        cutoutPercentage: 10,
+        rotation:	-0.5 * Math.PI,
+        circumference:	1 * Math.PI
+    }
 }]
 
 function floatToUnit(value, unit) {
@@ -155,101 +174,14 @@ function renderChart(graph) {
                 labels: graph.labels,
                 datasets: graph.datasets
             },
+            options: graph.options
         });
     }
     
 }
 
-function resetZoom(){
-
-    var ctx8 = document.getElementById("myChart8").getContext('2d');
-
-    var myChart8 = new Chart(ctx8, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                data: data4,
-                borderColor: 'rgba(124, 109, 109, 1)',
-                backgroundColor: 'rgba(124, 109, 109, 0.2)',
-            }]
-        },
-        options: {
-            // Elements options apply to all of the options unless overridden in a dataset
-            // In this case, we are setting the border of each bar to be 2px wide and green
-            elements: {
-                rectangle: {
-                    borderWidth: 0,
-                    borderSkipped: 'bottom'
-                }
-            },
-            responsive: true,
-            legend: {
-                display: false,
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'LOADS'
-            },
-
-            plugins: {
-                zoom: {
-                    pan: {
-                        enabled: true,
-                        mode: 'y'
-                    },
-                    zoom: {
-                        enabled: true,
-                        mode: 'y'
-                    },
-
-                    rangeMin: {
-                        // Format of min zoom range depends on scale type
-                        y: 0
-                    },
-                    rangeMax: {
-                        // Format of max zoom range depends on scale type
-                        y: 160000
-                    }
-                }
-            },
-            scales: {
-                yAxes: [
-                    {
-                        stacked: true,
-                        gridLines: {
-                            display: true
-                        },
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Consumption'
-                          },
-                        ticks: {
-                            display: true,
-                            beginAtZero: true,
-                            max: 60000,
-                            callback: function (value, index, values) {
-                                return float2dollar(value);
-                            }
-                        }
-                    }
-                ],
-                xAxes: [{
-                    stacked: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Time'
-                      },
-                    ticks: {
-                        display: true,
-                        min: 'January-17',
-                        max: 'December-17'
-                    }
-                }]
-            }
-        }
-    });
+function resetZoom(i){
+    var chart = renderChart(graphs[i])
     myChart8.resetZoom()
 }
 
